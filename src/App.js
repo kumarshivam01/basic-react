81cb762457bf49e6ffe9b9e146fda634cefc06c0
Kumar Shivam <98182315+kumarshivam01@users.noreply.github.com>
2022-04-13 23:45:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="electronics">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufucture</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  );
+}
